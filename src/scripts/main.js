a8e6a5b22e94fac5c42bf5ae4189938ef656e92d
Alvaro Bravo <54961568+AlvaroOk007@users.noreply.github.com.>
2024-05-03 11:44:00 -0300

    Agregando navegador principal y menu de busqueda

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,14 +23,30 @@ let estaMostrandoMenuPrincipal = false
 btnMostrarMenuPrincipal.addEventListener("click",()=>{
     if(estaMostrandoMenuPrincipal){
         menuPrincipal.classList.remove("posicionar-menu")
+        menuPrincipal.classList.add("ocultar-menu")
         estaMostrandoMenuPrincipal = false
         btnMostrarMenuPrincipal.innerText = "menu"
-        btnMostrarMenuPrincipal.classList.remove("posicionar-menu-hamburguesa")
-
     }else{
         menuPrincipal.classList.add("posicionar-menu")
+        menuPrincipal.classList.remove("ocultar-menu")
         estaMostrandoMenuPrincipal = true
         btnMostrarMenuPrincipal.innerText = "close"
-        btnMostrarMenuPrincipal.classList.add("posicionar-menu-hamburguesa")
     }
+})
+let menuBusqueda = document.getElementById("contenedor-busqueda")
+let btnBuscar = document.getElementById("btn-search")
+let btnCerrar = document.getElementById("btn-cerrar-search")
+let mostrandoMenuBusqueda = false
+btnBuscar.addEventListener("click",()=>{
+    if(!mostrandoMenuBusqueda){
+        menuBusqueda.classList.add("posicionar-menu")
+        menuBusqueda.classList.remove("ocultar-menu")
+        mostrandoMenuBusqueda = false
+    }
+})
+btnCerrar.addEventListener("click",()=>{
+    
+    menuBusqueda.classList.remove("posicionar-menu")
+    menuBusqueda.classList.add("ocultar-menu")
+    mostrandoMenuBusqueda = false
 })
